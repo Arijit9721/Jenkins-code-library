14996d5c78f8cf6f2272ca6bd087249c7b169ff9
Arijit Das <124693190+Arijit9721@users.noreply.github.com>
2025-02-26 23:49:31 +0530

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,8 +1,14 @@
 // to run this command you need to create credentials in jenkins 
-def call(string credential){
-withCredentials([usernamePassword(credentialsId: credential,
-passwordVariable: 'dockerHubPass', userVariable: 'dockerHubUser')]){
-sh 'docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}'
-sh 'docker tag notes-app:latest ${env.dockerHubUser}/notes-app:latest'
-sh 'docker push ${env.dockerHubUser}/notes-app:latest'
+def call(String credId, String imageName){
+  withCredentials([usernamePassword(
+                    credentialsId:"${credId}",
+                    passwordVariable: "dockerHubPass",
+                    usernameVariable: "dockerHubUser"
+                )]){
+                
+                sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
+                sh "docker image tag ${imageName} ${env.dockerHubUser}/${imageName}"
+                sh "docker push ${env.dockerHubUser}/${imageName}:latest"
+            
+                }  
 }
